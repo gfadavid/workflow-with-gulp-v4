54209301eac5199c8a3ae303743f512eebdab2f8
Angel González <gfadavid240@gmail.com>
2023-03-31 23:41:13 -0400

    Proyecto de ruleta (casino). Entorno listo.

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ var reload = browsersync.reload;
 gulp.task("browser-sync", function (done) {
   browsersync.init({
     server: "./",
-    startPath: "site-lol/dist/index.html", // After it browser running [File path set]
+    startPath: "site-royale/dist/index.html", // After it browser running [File path set]
     //    browser: 'chrome',
     host: 'localhost',
     //    port: 4000,
@@ -35,12 +35,12 @@ gulp.task("browser-sync", function (done) {
 // CSS task
 gulp.task("css", () => {
   return gulp
-    .src("site-lol/dev/scss/**/*")
+    .src("site-royale/dev/scss/**/*")
     .pipe(plumber())
     .pipe(sass({ outputStyle: "expanded" }))
     .pipe(rename({ suffix: ".min" }))
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest("site-lol/dist/css"))
+    .pipe(gulp.dest("site-royale/dist/css"))
     .pipe(notify({
       message: "main SCSS processed"
     }))
@@ -62,13 +62,13 @@ gulp.task("js", () => {
       .src([
         // '~/bootstrap/dist/js/bootstrap.bundle.js',
         'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
-        'site-lol/dev/js/scripts.js'
+        'site-royale/dev/js/scripts.js'
       ])
       .pipe(plumber())
 
       // folder only, filename is specified in webpack config
       .pipe(concat('scripts.js'))
-      .pipe(gulp.dest("site-lol/dist/js"))
+      .pipe(gulp.dest("site-royale/dist/js"))
       .pipe(browsersync.stream())
       .pipe(livereload())
   );
@@ -77,8 +77,8 @@ gulp.task("js", () => {
 // gulp.task("default", gulp.series( "css", "js", "webfonts", "browser-sync", () => {
 gulp.task("default", gulp.series("css", "js", "browser-sync", () => {
   livereload.listen();
-  gulp.watch(["site-lol/dev/scss/**/*"], gulp.series("css"));
-  gulp.watch(["site-lol/dev/js/**/*"], gulp.series("js"));
+  gulp.watch(["site-royale/dev/scss/**/*"], gulp.series("css"));
+  gulp.watch(["site-royale/dev/js/**/*"], gulp.series("js"));
   //   gulp.watch(["assets/scss/vendor/fontawesome/webfonts/*"], gulp.series("webfonts"));
 }));
 
